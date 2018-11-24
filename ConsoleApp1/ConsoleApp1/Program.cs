d0dd0d46c06aff5f88f6c6503ae88c6572980ce9
Daniel Sobczak <danielsobczak65@gmail.com>
2018-11-24 19:54:27 +0100

    Created first console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write((char)7);
+            Thread.Sleep(8000);
+        }
+    }
+}
