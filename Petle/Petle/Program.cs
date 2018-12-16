1b1dce18b2348b0cd80b3ccecb9a396d16caa4c4
Daniel Sobczak <danielsobczak65@gmail.com>
2018-12-16 19:34:36 +0100

    Classes "Loops"

---
+++ Petle/Petle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("{0}", i);
+
+                i++;
+            }
+            i = 0;
+            do
+            {
+                Console.WriteLine("{0}", i);
+
+                i++;
+            } while (i < 10);
+            */
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }           
+            Console.ReadKey();
+        }
+    }
+}
--- loteria/loteria/Program.cs
+++ loteria/loteria/Program.cs
@@ -11,44 +11,69 @@ namespace loteria
     {
         static void Main(string[] args)
         {
-            Console.Write("Podaj imie: \n");
-            string Imie = Console.ReadLine();
-
-            Console.Write("Podaj nazwisko: \n");
-            string Nazwisko = Console.ReadLine();
-
-            Console.Write("Podaj wiek: \n");
-            int Wiek = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Podaj miasto, w ktorym mieszkasz: \n");
-            string Miasto = Console.ReadLine();
-
-            Console.Write("Podaj numer telefonu: \n");
-            long Telefon = Convert.ToInt64(Console.ReadLine());
-
-            Console.Write("Podaj numer PESEL: \n");
-            long Pesel = Convert.ToInt64(Console.ReadLine());
-
-            Console.Write("Twoje dane osobowe: {0} {1}, lat {2}, zamieszkaly w miescie {3}, " +
-                "Numer telefonu: {4}, Numer PESEL: {5} \n", Imie, Nazwisko, Wiek, Miasto, Telefon, Pesel);
-
-            if (Wiek >= 18)
+            string warunek;
+            do
             {
-                Console.Write("Jestes dorosly \n");
-            }
-            String[] strings = { Imie };
-            foreach (var value in strings)
-            {
-                bool OstatniaLitera = value.EndsWith("a");
-                if (OstatniaLitera == true)
-                    if (Wiek >= 18)
-                        if (Miasto == "Warszawa")
+                Console.Write("Podaj imie: \n");
+                string imie = Console.ReadLine();
+
+                Console.Write("Podaj nazwisko: \n");
+                string nazwisko = Console.ReadLine();
+
+                Console.Write("Podaj wiek: \n");
+                int wiek = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Podaj miasto, w ktorym mieszkasz: \n");
+
+                string miasto;
+                do
                 {
-                    Console.WriteLine("Wygrales na loterii!");
-                }
-                    }
+                    miasto = Console.ReadLine();
 
-            Thread.Sleep(50000);
+                } while (!(miasto == "Warszawa" || miasto == "Szczecin" || miasto == "Bytom"));
+
+                Console.Write("Podaj numer telefonu: \n");
+                string telefon = Console.ReadLine();
+                while (telefon.Length != 9)
+                {
+                    Console.Write("Podales zly numer. Podaj jeszcze raz\n");
+                    telefon = Console.ReadLine();
+                }
+
+
+                Console.Write("Podaj numer pesel: \n");
+                long pesel = Convert.ToInt64(Console.ReadLine());
+
+                Console.Write("Twoje dane osobowe: {0} {1}, lat {2}, zamieszkaly w miescie {3}, " +
+                    "Numer telefonu: {4}, Numer pesel: {5} \n", imie, nazwisko, wiek, miasto, telefon, pesel);
+
+                if (wiek >= 18)
+                {
+                    Console.Write("Jestes dorosly \n");
+                }
+                String[] strings = { imie };
+                foreach (var value in strings)
+                {
+                    bool OstatniaLitera = value.EndsWith("a");
+                    if (OstatniaLitera == true)
+                        if (wiek >= 18)
+                            if (miasto == "Warszawa") 
+                            {
+                                Console.WriteLine("Wygrales na loterii!");
+                            }
+                }
+
+                Console.Write("Czy chcesz kontynuowac? (t\\n) ");
+                warunek = Console.ReadKey().KeyChar.ToString(); //consolekeyinfo to char to string
+                Console.WriteLine();
+                //Console.Write(warunek + "\n"); //konkatenacja
+            } while (warunek != "n");
         }
     }
 }
+/* 
+ * Uzytkownik podaje dwie liczby. Nastepnie wybiera czy ma byc pionowo czy poziomo, a program rysuje prostokat z podanych wartosci w odpowiednia strone. prostokat ma byc wykonany z "#"
+ * w pionie nie moze byc > 6 a w poziomie > 8 
+ * 2. prostokat ma byc pusty w srodku (w srodku maja byc spacje). uzytkownik wybiera czy ma byc pusty czy wypelniony #
+ * 
+ */
